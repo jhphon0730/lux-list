b169197bcd6fcef9602fb7abf754274aa4e20744
jeongho0730 <eeung025@gmail.com>
2025-05-16 14:51:06 +0900

    feat: 사용자 인증 메서드 추가 및 라우터 등록 수정

--- internal/controller/auth-controller.go
+++ internal/controller/auth-controller.go
@@ -7,7 +7,10 @@ import (
 )
 
 // AuthController는 사용자 인증 관련 메서드를 정의하는 인터페이스
-type AuthController interface{}
+type AuthController interface {
+	login(c *gin.Context)
+	logout(c *gin.Context)
+}
 
 // authController는 AuthController 인터페이스를 구현하는 구조체
 type authController struct {
@@ -15,8 +18,8 @@ type authController struct {
 }
 
 func RegisterRoutes(router *gin.RouterGroup, authController AuthController) {
-	router.POST("/login", nil)
-	router.GET("/logout", nil)
+	router.POST("/login", authController.login)
+	router.GET("/logout", authController.logout)
 }
 
 // NewAuthController는 AuthController의 인스턴스를 생성하는 함수
@@ -25,3 +28,9 @@ func NewAuthController(authService service.AuthService) AuthController {
 		authService: authService,
 	}
 }
+
+// login은 사용자 로그인 요청을 처리하는 메서드
+func (c *authController) login(ctx *gin.Context) {}
+
+// logout은 사용자 로그아웃 요청을 처리하는 메서드
+func (c *authController) logout(ctx *gin.Context) {}
