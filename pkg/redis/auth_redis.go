ac4686b6e1ea4294255f40ab454b4493170d8264
jeongho0730 <eeung025@gmail.com>
2025-05-19 09:51:36 +0900

    feat: Redis 클라이언트 초기화 및 반환을 위한 sync.Once 인스턴스 추가

--- pkg/redis/auth_redis.go
+++ pkg/redis/auth_redis.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"lux-list/internal/config"
 	"lux-list/pkg/utils"
+	"sync"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -10,6 +11,8 @@ import (
 var (
 	// auth_redis는 Redis 클라이언트 인스턴스
 	auth_redis *redis.Client
+	// auth_redis_once는 Redis 클라이언트 초기화를 위한 sync.Once 인스턴스
+	auth_redis_once sync.Once
 )
 
 // InitAuthRedis는 Redis 클라이언트를 초기화하고 반환하는 함수
@@ -21,5 +24,16 @@ func InitAuthRedis() *redis.Client {
 		DB:       utils.InterfaceToInt(config.Redis.AuthDB),
 	})
 
+	return auth_redis
+}
+
+// GetAuthRedis는 Redis 클라이언트를 반환하는 함수
+func GetAuthRedis() *redis.Client {
+	// auth_redis_once는 한 번만 실행되도록 보장하는 sync.Once 인스턴스
+	auth_redis_once.Do(func() {
+		if auth_redis == nil {
+			auth_redis = InitAuthRedis()
+		}
+	})
 	return auth_redis
 }
