4fcd082a85f9a4a6b4eb727f29309879f0253d84
jeongho0730 <eeung025@gmail.com>
2025-06-13 14:08:50 +0900

    feat: Task 생성 Request 구조체 추가가

--- internal/model/task.go
+++ internal/model/task.go
@@ -10,7 +10,15 @@ type Task struct {
 	Description *string   `db:"description"`
 	DueDate     time.Time `db:"due_date"`
 	IsCompleted bool      `db:"is_completed"`
-	Priority    string    `db:"priority"`
+	Priority    string    `db:"priority"` // "low", "medium", "high"
 	CreatedAt   time.Time `db:"created_at"`
 	UpdatedAt   time.Time `db:"updated_at"`
+}
+
+type CreateTaskRequest struct {
+	Title       string    `json:"title"`
+	Description *string   `json:"description"`
+	DueDate     time.Time `json:"due_date"`
+	IsCompleted bool      `json:"is_completed"`
+	Priority    string    `json:"priority"` // "low", "medium", "high"
 }
