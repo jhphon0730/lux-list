20ebeee13d58c3194fc2cddb64272e545ff5fe94
jeongho0730 <eeung025@gmail.com>
2025-06-19 16:30:19 +0900

    feat: 태그 서비스 메서드 구현 및 오류 처리 추가 - GetTags, GetTagsByTagID, GetTagsByUserID, GetTagsByTaskID 메서드 추가 - 태그 조회 시 오류 처리 로직 개선

--- internal/service/tag_service.go
+++ internal/service/tag_service.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"database/sql"
+	"errors"
 	"lux-list/internal/model"
 	"lux-list/internal/repository"
+	"net/http"
 )
 
 // TagService는 태그 관련 메서드를 정의하는 인터페이스
@@ -23,4 +26,43 @@ func NewTagService(tagRepository repository.TagRepository) TagService {
 	return &tagService{
 		tagRepository: tagRepository,
 	}
+}
+
+// GetTags는 사용자의 모든 태그를 조회하는 메서드
+func (s *tagService) GetTags(userID int) ([]model.Tag, int, error) {
+	tags, err := s.tagRepository.GetTags(userID)
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+	return tags, http.StatusOK, nil
+}
+
+// GetTagsByTagID는 사용자의 특정 태그를 조회하는 메서드
+func (s *tagService) GetTagsByTagID(userID int, tagID int) (*model.Tag, int, error) {
+	tag, err := s.tagRepository.GetTagsByTagID(userID, tagID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, http.StatusNotFound, errors.New("tag not found")
+		}
+		return nil, http.StatusInternalServerError, err
+	}
+	return tag, http.StatusOK, nil
+}
+
+// GetTagsByUserID는 사용자의 모든 태그를 조회하는 메서드
+func (s *tagService) GetTagsByUserID(userID int) ([]model.Tag, int, error) {
+	tags, err := s.tagRepository.GetTagsByUserID(userID)
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+	return tags, http.StatusOK, nil
+}
+
+// GetTagsByTaskID는 특정 작업에 연결된 태그를 조회하는 메서드
+func (s *tagService) GetTagsByTaskID(userID int, taskID int) ([]model.Tag, int, error) {
+	tags, err := s.tagRepository.GetTagsByTaskID(userID, taskID)
+	if err != nil {
+		return nil, http.StatusInternalServerError, err
+	}
+	return tags, http.StatusOK, nil
 }
