cfe6527932e553c24a12189a70f92e14a3fcd6ad
jeongho0730 <eeung025@gmail.com>
2025-06-19 17:19:10 +0900

    feat: 태그 조회 메서드 구현 및 오류 처리 추가 - GetTagsByTagID 메서드에서 사용자 ID 및 태그 ID 유효성 검사 추가 - 태그 조회 결과를 JSON 형식으로 반환하도록 수정

--- internal/controller/tag_controller.go
+++ internal/controller/tag_controller.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"lux-list/internal/service"
+	"lux-list/pkg/utils"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -34,12 +36,31 @@ func NewTagController(tagService service.TagService) TagController {
 
 // GetTagsByTagID는 태그 ID로 태그를 조회하는 메서드
 func (c *tagController) GetTagsByTagID(ctx *gin.Context) {
+	userID, err := utils.GetUserIDFromContext(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+
+	tagID := ctx.Param("tagID")
+	if tagID == "" {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Tag ID is required"})
+		return
+	}
+
+	tag, status, err := c.tagService.GetTagsByTagID(userID, utils.InterfaceToInt(tagID))
+	if err != nil {
+		ctx.JSON(status, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(status, gin.H{"tag": tag})
 }
 
 // GetTagsByUserID는 사용자의 모든 태그를 조회하는 메서드
 func (c *tagController) GetTagsByUserID(ctx *gin.Context) {
 }
 
-// GetTagsByTaskID는 특정 작업에 연결된 태그를 조회하는 메서드
+// GetTagsByTaskID는 작업 ID로 태그를 조회하는 메서드
 func (c *tagController) GetTagsByTaskID(ctx *gin.Context) {
 }
